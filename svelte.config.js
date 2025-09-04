91f9610353847a133b3f8ca7c1635bee6a5af8a6
Shubham Bhattacharya <DrishtiWeb@Drishti-Web.local>
2025-09-05 01:34:41 +0530

    SvelteKit: set base+assets, use index.html fallback

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 const dev = process.argv.includes('dev');
 
 export default {
   kit: {
-    adapter: adapter({ fallback: '404.html' }),
-    paths: { base: dev ? '' : '/r-r-dash' }
+    adapter: adapter({
+      // we'll copy index.html to 404.html in the workflow
+      fallback: 'index.html'
+    }),
+    paths: {
+      // repo base path
+      base: dev ? '' : '/r-r-dash',
+      // ensure _app asset URLs are under the same base
+      assets: dev ? '' : '/r-r-dash'
+    }
   }
 };
